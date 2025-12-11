1374351e06ea73eaa0624b02da490a88e80aa9c4
CharlosFerdinand <74392017+CharlosFerdinand@users.noreply.github.com>
2025-12-11 23:21:13 +0700

    Commit No. 2 – update to Room

--- app/src/main/java/com/example/test_lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieApplication.kt
@@ -2,20 +2,29 @@ package com.example.test_lab_week_13
 
 import android.app.Application
 import com.example.test_lab_week_13.api.MovieService
+import com.example.test_lab_week_13.database.MovieDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MovieApplication : Application() {
+
     lateinit var movieRepository: MovieRepository
+
     override fun onCreate() {
         super.onCreate()
+
+        // Build Retrofit
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
-        val movieService = retrofit.create(
-            MovieService::class.java
-        )
-        movieRepository = MovieRepository(movieService)
+
+        val movieService = retrofit.create(MovieService::class.java)
+
+        // ✅ Step 1: Create MovieDatabase instance
+        val movieDatabase = MovieDatabase.getInstance(applicationContext)
+
+        // ✅ Step 2: Pass BOTH service + database to repository
+        movieRepository = MovieRepository(movieService, movieDatabase)
     }
 }
--- app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
@@ -1,19 +1,31 @@
 package com.example.test_lab_week_13
 
 import com.example.test_lab_week_13.api.MovieService
+import com.example.test_lab_week_13.database.MovieDao
+import com.example.test_lab_week_13.database.MovieDatabase
 import com.example.test_lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
+class MovieRepository(
+    private val movieService: MovieService,
+    private val movieDatabase: MovieDatabase
+) {
     private val apiKey = "b63fa711c512305f1f5ff10ada88fa3e"
 
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-            emit(movieService.getPopularMovies(apiKey).results)
+            val movieDao: MovieDao = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+            if(savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+                movieDao.addMovies(movies)
+                emit(movies)
+            } else {
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
     }
-
 }
